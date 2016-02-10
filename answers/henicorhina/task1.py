58873130be28d32b64b51c440a0d0a09d29470c0
OSCAR JOHNSON <OJOHNS7@TIGERS.LSU.EDU>
2016-02-10 08:41:09 -0600

    task1 complete

--- answers/henicorhina/task1.py
+++ answers/henicorhina/task1.py
@@ -2,21 +2,32 @@
 # -*- coding: utf-8 -*-
 
 """
-BIOL7800 assignment 6
-Oscar Johnson 9 February 2016
+BIOL7800 assignment 7
+Oscar Johnson 10 February 2016
 """
 
 
-def math ():
-    x = input ("is coding fun? ")
-    if x == "y":
-        print ("yeah!!")
-    elif x == "n":
-        print ("it's time to change careers")
+def palindrome(string):
+    """
+    function takes a string and proves whether or not 
+    it is a palindrome
+    """
+    string = string.replace(" ", "") # removes whitespace
+    if string[::-1] == string:
+        #print (string)        
+        return "this is a palindrome"
 
+    elif string[::-1] != string:
+        #print (string)        
+        return "this is not a palindrome"
+    
+    else:
+        return "you have failed somehow"
+        
 
 def main():
-    math()
+    x = str(input ("what is your palindrome? "))
+    print (palindrome(x))
 
 
 if __name__ == '__main__':
