098e324684dfb1061827aab84f4b33e83752ba30
OSCAR JOHNSON <OJOHNS7@TIGERS.LSU.EDU>
2016-02-10 08:27:16 -0600

    first commit
    just getting everything set up

---
+++ answers/henicorhina/task1.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+BIOL7800 assignment 6
+Oscar Johnson 9 February 2016
+"""
+
+
+def math ():
+    x = input ("is coding fun? ")
+    if x == "y":
+        print ("yeah!!")
+    elif x == "n":
+        print ("it's time to change careers")
+
+
+def main():
+    math()
+
+
+if __name__ == '__main__':
+    main()
+
