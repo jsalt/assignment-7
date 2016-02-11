e796f8b578068362f6a89d82bf11b9065eaa7702
sthshraddha <sshre13@lsu.edu>
2016-02-10 22:25:16 -0600

    assignment_7

---
+++ answers/sthshraddha/Task2_just_for_fun.py
@@ -0,0 +1,18 @@
+"""
+fix_me.py originaly was a recursive program that is meant to print each base of
+a given input sequence to the screen, one base per line. This program is
+modified to get the result without recursion.
+
+Created by Brant Faircloth on 03 February 2016.
+Copyright 2016 Brant C. Faircloth. All rights reserved.
+
+Corrected by Shraddha Shrestha on 8th February 2016.
+
+"""
+
+position=0
+sequence="ATCGCGTAGCACGACTCTGCTCGC"
+len(sequence)
+letter = sequence[position:]
+print("\n".join(letter))
+position = position + 1
